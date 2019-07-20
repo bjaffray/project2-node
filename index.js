23cb238d8736d960a80f5d126d4bf2ac604b63c9
bjaffray <jaf15001@byui.edu>
2019-07-20 17:22:46 -0600

    change

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const PORT = process.env.PORT || 5000
 require('dotenv').config();
 const mainController = require("./Controller/mainController.js");
 
-
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))

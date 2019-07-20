d289d6c3175fba5d1e9564c941ae58ddb8032ad2
bjaffray <jaf15001@byui.edu>
2019-07-20 17:16:30 -0600

    helpme

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+require('dotenv').config();
 const mainController = require("./Controller/mainController.js");
 
 express()

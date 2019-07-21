7eea9186362e481a23e6ff24745c7ed2138bef3f
bjaffray <jaf15001@byui.edu>
2019-07-20 18:51:21 -0600

    heyo

--- public/clientside.js
+++ public/clientside.js
@@ -16,16 +16,16 @@ function checkInfo(address)
 
 function displayItems()
 {
-    console.log("starting");
-
     $.get("/getItems", function(data) {
-        console.log("Running");
+        $("#displayItems_id").empty();
+
+        $("#displayItems_id").append("<tr><th>Item</th><th></th><th>Description</th><th>Price (Gold)</th><th>Items in Cart</th></tr>");
 
         for (var i = 0; i < data.list.length; i++) {
             var item = data.list[i];
             if(i == 0){
                 itemID = item.item_id;
-                $("#displayItems_id").empty().append("<tr onclick=\"checkInfo(" + i + ")\"> <td>" + item.ItemName + "<td><img src=\"" + item.PicPath + "\" alt=\"" + item.ItemName + "\"> <td>" + item.Description + "<td>" + item.ItemPrice + "<td><input type=\"checkbox\" name=\"cart" + i + "\" id=\"" + i + "\" value=\"1\"");
+                $("#displayItems_id").append("<tr onclick=\"checkInfo(" + i + ")\"> <td>" + item.ItemName + "<td><img src=\"" + item.PicPath + "\" alt=\"" + item.ItemName + "\"> <td>" + item.Description + "<td>" + item.ItemPrice + "<td><input type=\"checkbox\" name=\"cart" + i + "\" id=\"" + i + "\" value=\"1\"");
             }
             else{
                 itemID = item.item_id;

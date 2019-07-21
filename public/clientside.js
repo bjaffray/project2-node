1f5308496b2bef7f2da1685c024d31d92976abbd
bjaffray <jaf15001@byui.edu>
2019-07-20 18:48:40 -0600

    edits

--- public/clientside.js
+++ public/clientside.js
@@ -25,48 +25,12 @@ function displayItems()
             var item = data.list[i];
             if(i == 0){
                 itemID = item.item_id;
-                $("#displayItems_id").empty().append("<tr onclick=\"checkInfo(" + i + ")\"> <td>" + item.ItemName + "<td><img src=\"" + item.PicPath + "\" alt=\"" + item.ItemName + "\"> + <td>" + item.Description + "<td>" + item.ItemPrice + "<td><input type=\"checkbox\" name=\"cart" + i + "\" id=\"" + i + "\" value=\"1\"");
+                $("#displayItems_id").empty().append("<tr onclick=\"checkInfo(" + i + ")\"> <td>" + item.ItemName + "<td><img src=\"" + item.PicPath + "\" alt=\"" + item.ItemName + "\"> <td>" + item.Description + "<td>" + item.ItemPrice + "<td><input type=\"checkbox\" name=\"cart" + i + "\" id=\"" + i + "\" value=\"1\"");
             }
             else{
                 itemID = item.item_id;
-                $("#displayItems_id").append("<tr onclick=\"checkInfo(" + i + ")\"> <td>" + item.ItemName + "<td><img src=\"" + item.PicPath + "\" alt=\"" + item.ItemName + "\"> + <td>" + item.Description + "<td>" + item.ItemPrice + "<td><input type=\"checkbox\" name=\"cart" + i + "\" id=\"" + i + "\" value=\"1\"");
+                $("#displayItems_id").append("<tr onclick=\"checkInfo(" + i + ")\"> <td>" + item.ItemName + "<td><img src=\"" + item.PicPath + "\" alt=\"" + item.ItemName + "\"> <td>" + item.Description + "<td>" + item.ItemPrice + "<td><input type=\"checkbox\" name=\"cart" + i + "\" id=\"" + i + "\" value=\"1\"");
             }
         }
     })
 }
-
-
-
-{/*             <tr>
-                <th>Item</th>
-                <th></th>
-                <th>Description</th>
-                <th>Price (Gold)</th>
-                <th>Items in Cart</th>
-            </tr>
-            <!-- <?php
-            for ($i = 0; $i < sizeof($_SESSION['store_items']) - 1; $i++) {
-                $to_print = $_SESSION['store_items'][$i];
-                echo "<tr onclick=\"checkInfo($i)\">";
-                echo "<td>" . $to_print->name;
-                echo "<td><img src=\"$to_print->filep\" alt=\"$to_print->name\">";
-                echo "<td>" . $to_print->desc;
-                echo "<td>" . $to_print->price;
-                echo "<td><input type=\"checkbox\" name=\"cart$i\" id=\"$i\" value=\"1\"";
-                if ($to_print->in_cart == 1) echo "checked";
-                echo "onclick=\"checkInfo($i)\">";
-            }
-            ?> 
-        
-        
-        <tr onclick="checkInfo(" + i + ")"> +
-        <td> + item.ItemName +
-        <td><img src=\"" + item.PicPath + "\" alt=\"" + item.ItemName + "\"> +
-        <td> + item.Description +
-        <td> + item.ItemPrice +
-        <td><input type=\"checkbox\" name=\"cart" + i + "\" id=\"" + i + "\" value="1"
-        
-        
-        
-        --> */}
-
